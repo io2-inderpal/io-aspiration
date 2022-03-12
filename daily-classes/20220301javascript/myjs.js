9b54b09ad80eef6f12e1923976e876008252602e
io2-inderpal <io2.inderpal@gmail.com>
2022-03-12 07:55:07 +0530

    javascript12-03

--- daily-classes/20220301javascript/myjs.js
+++ daily-classes/20220301javascript/myjs.js
@@ -198,3 +198,35 @@
 // } else {
 //   ("not a valid number");
 // }
+
+//write a program that receiver month and date of birth as input and
+// print the corresponding zodiac sign on the following table
+const m = prompt("type any month with number 1 to 12");
+const d = prompt("type a any date ");
+if ((m == 12 && date <= 22) || (m == 1 && date >= 19)) {
+  console.log(m + "/" + d, " is capricorn");
+} else if ((m == 1 && d <= 20) || (m == 2 && d >= 17)) {
+  console.log(m + "/" + d, "is aquarius");
+} else if ((m == 2 && d <= 18) || (m == 3 && d >= 19)) {
+  console.log(m + "/" + d, "is pisces");
+} else if ((m == 3 && d <= 20) || (m == 4 && d >= 19)) {
+  console.log(m + "/" + d, "is aries");
+} else if ((m == 4 && d <= 20) || (m == 5 && d >= 20)) {
+  console.log(m + "/" + d, "is taurus");
+} else if ((m == 5 && d <= 21) || (m == 6 && d >= 20)) {
+  console.log(m + "/" + d, "is gemini");
+} else if ((m == 6 && d <= 21) || (m == 7 && d >= 22)) {
+  console.log(m + "/" + d, "cancer");
+} else if ((m == 7 && d <= 23) || (m == 8 && d >= 22)) {
+  console.log(m + "/" + d, "is leo");
+} else if ((m == 8 && d <= 23) || (m == 9 && d >= 22)) {
+  console.log(m + "/" + d, "is virgo");
+} else if ((m == 9 && d <= 23) || (m == 10 && d >= 22)) {
+  console.log(m + "/" + d, "is libra");
+} else if ((m == 10 && d <= 23) || (m == 11 && d >= 21)) {
+  console.log(m + "/" + d, "is scorpio");
+} else if ((m == 11 && d <= 22) || (m == 12 && d >= 21)) {
+  console.log(m + "/" + d, "is sagittarius");
+} else {
+  console.log(m + "/" + d, "this is wrong input");
+}
